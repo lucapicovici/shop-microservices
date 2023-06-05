b72e7f3b1315cc62c4d109c7486a12cf14acdf51
lucapicovici <lucapicovici79@gmail.com>
2023-06-06 02:29:31 +0300

    Add orders deployment files

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -14,7 +14,6 @@ import { newOrderRouter } from './routes/new';
 import { showOrderRouter } from './routes/show';
 
 const app = express();
-app.set('trust proxy', true);
 app.use(json());
 app.use(
   cookieSession({
